daa44b88f00432550a15b3c6157cae3c067c28e1
Bidhit <bidhitkumar61@gmail.com>
2025-06-14 10:02:43 +0530

    Add weather activity suggestion script

---
+++ 02_Conditionals/WeatherActivity.py
@@ -0,0 +1,11 @@
+# Question:05 : Weather Activity Suggestion
+# Statement: Suggest an activity based on the weather (Eg. Sunny - Go for a walk, Rainy - Read a book, Snowy - Build a snowman)
+
+Weather = str(input("Enter the Weather name: "))
+
+if(Weather == "Sunny"):
+    print("Go for a walk")
+elif(Weather == "Rainy"):
+    print("Read a book")
+elif(Weather == "Snowy"):
+    print("Build a snowman")
