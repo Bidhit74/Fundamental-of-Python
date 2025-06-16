150dcdc18296a41739d165816220a6eb0fb95913
Bidhit <bidhitkumar61@gmail.com>
2025-06-16 20:41:28 +0530

    Refactor basics_function.py to include order_summary function and clean up commented code

--- 05_Functions/basics_function.py
+++ 05_Functions/basics_function.py
@@ -14,10 +14,24 @@
 # print(sum(numbers)) # integer are coerced to float
 
 # without import in buit function
-print(abs(-5)) 
-print(round(3.124))
-print(round(3.124345,2)) # round(x, ndigit)
-print(max(1,2,34,5,39)) # return maximum value
-print(min(1,2,34,5,39)) # return minimum value
-print(pow(2,5)) # pow(x,y) return x power y
-print(sum([1,2,3,4]))
+# print(abs(-5)) 
+# print(round(3.124))
+# print(round(3.124345,2)) # round(x, ndigit)
+# print(max(1,2,34,5,39)) # return maximum value
+# print(min(1,2,34,5,39)) # return minimum value
+# print(pow(2,5)) # pow(x,y) return x power y
+# print(sum([1,2,3,4]))
+
+# Combining positional,Keyword and arbitery argument
+
+def order_summary(order_id, *items, **customer_info):
+    print(f"Order ID: {order_id}")
+    print("Items Ordered:")
+    for item in items:
+        print(f"- {item}")
+    
+    print("Customer Information:")
+    for key, value in customer_info.items():
+        print(f"{key}: {value}")
+
+order_summary(101,"apple","banana","orange", name="John Doe", address="123 Main St", phone="555-1234")
