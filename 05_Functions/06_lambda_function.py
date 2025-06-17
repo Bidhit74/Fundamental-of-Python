e04f47be9ab0b82dccff20f90df1c5a9a1bd50c4
Bidhit <bidhitkumar61@gmail.com>
2025-06-17 15:27:02 +0530

    Lambda Function.

---
+++ 05_Functions/06_lambda_function.py
@@ -0,0 +1,6 @@
+# Question 6 - Lambda Function.
+# Statement: Create a lambda function to compute the cube of a number.
+
+cube = lambda x: x**3  # short cut function syntax # single time use karne ke liye use hota hai
+
+print(cube(7))
