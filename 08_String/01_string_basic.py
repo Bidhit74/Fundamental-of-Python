13baa0776b61112fa52ac0d7e00e2e32f06a65cf
Bidhit <bidhitkumar61@gmail.com>
2025-06-18 16:46:47 +0530

    Add examples demonstrating string creation, access, and iteration in Python

---
+++ 08_String/01_string_basic.py
@@ -0,0 +1,34 @@
+# s1 = str() # create string using constructor
+# s1 = str("Hello")
+# print(type(s1))
+# print(s1)
+# other way create a string
+# name = 'ZBidhixt' # or "Bidhit" # Both are String
+
+# # In Buit function in python
+# print(len(name)) # length of string # 6
+# print(min(name)) # minimum character and uppre character in string 
+# print(max(name)) # maximum character and lower latter in string
+
+
+# Access the String
+name = 'Bidhit'
+print(name[0]) # first character
+print(name[-1]) # End character
+
+# Use for loop
+for ch in name:
+    print(ch,end=" ")
+
+print()
+# Use While loop
+
+index = 0
+while index < len(name):
+    print(name[index], end=" ")
+    index += 1
+
+
+
+
+
