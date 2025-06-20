1ee2d3e203141840902b2e46410cd162094af359
Bidhit <bidhitkumar61@gmail.com>
2025-06-20 18:37:09 +0530

    Refactor Car class to encapsulate brand attribute and add display method

--- 10_Oops/05_Polymorphism.py
+++ 10_Oops/05_Polymorphism.py
@@ -3,32 +3,48 @@
 
 class Car:
     def __init__(self,brand,model):
-        self.__brand = brand
+        self.brand = brand
         self.model = model
 
-    def get_brand(self):
-        return self.__brand
-    
-    def set_brand(self,new_brand):
-        self.__brand = new_brand
-
-    def display(self):
-        print(f"Brand:- {self.__brand} and Model:- {self.model}")
-
-    def fuel_type(self):
+    def fuel_type(self): 
         return "Petrol or Diesel"
-
+    
 class ElectricCar(Car): # Inherit class Car
     def __init__(self, brand, model,battery_size):
         super().__init__(brand, model)
         self.battery_size = battery_size
     
-    def fuel_type(self):
+    def fuel_type(self): # Function Overriding (Inheritance)
         return "Electric Charge"
 
+# my_tesla = ElectricCar("Tesla", "Model", "85kWh")
+# print(my_tesla.fuel_type())
+# my_car = Car("Toyota","Carolla")
+# print(my_car.fuel_type())
 
-my_tesla = ElectricCar("Tesla", "Model", "85kWh")
-print(my_tesla.fuel_type())
-my_car = Car("Toyota","Carolla")
-print(my_car.fuel_type())
+# Type of polymorphism:  2. Duck Typing (No need to check type)
+class Cat:
+    def speak(self):
+        print("Meow")
+
+class Person:
+    def speak(self):
+        print("Hello")
+
+def call_speak(obj):
+    obj.speak()
+
+call_speak(Cat())     # Meow
+call_speak(Person())  # Hello
+#  call_speak() function sabhi objects ke liye kaam karta hai jisme speak() method hai.
+
+# Type of polymorphism🔹 3. Built-in Polymorphism (Operator Overloading)
+
+print(2 + 3)           # 5 → addition
+print("A" + "B")       # AB → string join
+print([1, 2] + [3, 4]) # [1, 2, 3, 4] → list merge
+
+# ✅ Same operator +, different behavior based on data type.
+
+
 
